f3f7f02ec4a448a9958ba13535d65faecfaa23a7
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-11-07 23:26:07 -0300

    Delete movies e tags

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -36,6 +36,14 @@ class MoviesController {
             tags
         })
     }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("movies").where({ id }).delete();
+
+        return response.json()
+    }
 }
 
 module.exports = MoviesController
