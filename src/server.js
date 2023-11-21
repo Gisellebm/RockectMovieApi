342003f90efcf99258e60333b822debc36a78ac6
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-11-21 20:27:24 -0300

    Preparando o Deploy

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors")
 
 const AppError = require('./utils/AppError')
@@ -36,5 +37,5 @@ app.use((error, request, response, next) => {
     })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
